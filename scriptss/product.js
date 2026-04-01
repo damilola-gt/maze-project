1fa94a52d8a278fc4b02fa8c740ec634cd814221
damilola-gt <damilolagithubpractice@gmail.com>
2026-04-01 16:41:52 +0100

    add correct file paths for images

--- scriptss/product.js
+++ scriptss/product.js
@@ -4,32 +4,32 @@
      { id: '0001',
        name: 'Teal T-shirt',
        priceCents: 2000,
-       image: '/images/adults-plain-cotton-tshirt-2-pack-teal.jpg'
+       image: '../images/adults-plain-cotton-tshirt-2-pack-teal.jpg'
      },
       { id: '0002',
        name: 'Backpack',
        priceCents: 3000,
-       image: '/images/backpack.jpg'
+       image: '../images/backpack.jpg'
      },
       { id: '0003',
        name: 'Bathroom rug',
        priceCents: 600,
-       image: '/images/bathroom-rug.jpg'
+       image: '../images/bathroom-rug.jpg'
      },
       { id: '0004',
        name: 'Black 2 slot toaster',
        priceCents: 4500,
-       image: '/images/black-2-slot-toaster.jpg'
+       image: '../images/black-2-slot-toaster.jpg'
       },
       { id: '0005',
        name: 'Double elongated earrings',
        priceCents: 308,
-       image: '/images/double-elongated-twist-french-wire-earrings.webp'
+       image: '../images/double-elongated-twist-french-wire-earrings.webp'
      },
       { id: '0006',
        name: 'blue cover-duvet set',
        priceCents: 1500,
-       image: '/images/duvet-cover-set-blue-twin.jpg'
+       image: '../images/duvet-cover-set-blue-twin.jpg'
      }
 
 
